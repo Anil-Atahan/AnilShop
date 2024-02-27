be01d4cad12c34ca0c19bc1231dfaad02e27cb00
Anıl Atahan <>
2024-02-27 18:52:38 +0300

    no message

---
+++ AnilShop.Api/Program.cs
@@ -0,0 +1,46 @@
+using System.Reflection;
+using AnilShop.Products;
+using AnilShop.Users;
+using FastEndpoints;
+using FastEndpoints.Security;
+using FastEndpoints.Swagger;
+using Serilog;
+
+var logger = Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+logger.Information("Starting web host");
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((_, config) =>
+    config.ReadFrom.Configuration(builder.Configuration));
+
+builder.Services.AddFastEndpoints()
+    .AddJWTBearerAuth(builder.Configuration["Auth:JwtSecret"]!)
+    .AddAuthorization()
+    .SwaggerDocument();
+
+List<Assembly> mediatRAssemblies = [typeof(Program).Assembly];
+builder.Services.AddProductsService(builder.Configuration, logger, mediatRAssemblies);
+builder.Services.AddUserModuleServices(builder.Configuration, logger, mediatRAssemblies);
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssemblies(mediatRAssemblies.ToArray()));
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseFastEndpoints();
+app.UseSwaggerGen();
+
+app.UseHttpsRedirection();
+
+app.UseFastEndpoints();
+
+app.Run();
+
+public partial class Program { }
---
+++ AnilShop.Products/Data/Migrations/20240225134951_Initial.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AnilShop.Products.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Products");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                schema: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "numeric(18,6)", precision: 18, scale: 6, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "Products",
+                table: "Products",
+                columns: new[] { "Id", "Description", "Price", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("17c61e41-3953-42cd-8f88-d3f698869b35"), "description", 12.99m, "title 3" },
+                    { new Guid("a89f6cd7-4693-457b-9009-02205dbbfe45"), "description", 10.99m, "title 1" },
+                    { new Guid("e4fa19bf-6981-4e50-a542-7c9b26e9ec31"), "description", 11.99m, "title 2" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "Products");
+        }
+    }
+}
