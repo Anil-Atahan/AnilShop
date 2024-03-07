82fbdd90e28381d571be1ce7d43b107f4a62a8cb
Anıl Atahan <anilatahan95@gmail.com>
2024-03-07 19:45:22 +0300

    qweqwe

---
+++ AnilShop.EmailSending/Endpoints/ListEmailsResponse.cs
@@ -0,0 +1,7 @@
+namespace AnilShop.EmailSending.Endpoints;
+
+public class ListEmailsResponse
+{
+    public int Count { get; set; }
+    public List<EmailOutboxEntity> Emails { get; internal set; } = new();
+}
